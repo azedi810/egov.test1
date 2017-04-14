5200f9fbfae9cd30eb49c289f548e42a01e40ef8
paid_jhk <paid_jhk@172.16.22.57>
2017-04-14 17:56:42 +0900

    게임

---
+++ egov.test1/src/main/java/example/game/lotto/ArrayListLotto.java
@@ -0,0 +1,56 @@
+package example.game.lotto;
+
+/* 
+ * 제작자 : 자바킹 
+ * 제작일 : 12.04.24 
+ * 제작환경 : win7 64bit , eclipse Indigo, JDK 6.0  
+ */ 
+import java.util.*; 
+
+public class ArrayListLotto extends ArrayList<String> { 
+    /** 
+     * 1. ArrayList를 상속받아 6개의 공간을 초기화 한다. 
+     */ 
+    public ArrayListLotto() { 
+        super(6); 
+    } 
+
+    /** 
+     * Random 수를 발생하여 ArrayListLotto; 
+     */ 
+    public void randomAdd() { 
+
+        int cnt = 0; 
+        while (cnt < 6) { 
+            String ran = Integer.toString(((int) (Math.random() * 45 + 1))); 
+            if (!contains(ran)) { 
+                add(ran); // 중복이 아니면 램덤값 ArrayList추가 
+                cnt++; // 중복값이 없으면 증가 
+            } 
+        } 
+    } 
+
+    /** 
+     * ArrayList에 저장된 난수의 값들을 올림차순으로 정렬한다. 
+     */ 
+    public void lottoSortAsc() { 
+        Collections.sort(this); // 오름차순 
+    } 
+
+    public void lottoSortDec() { 
+        Collections.sort(this, Collections.reverseOrder()); // 내림차순 
+
+    } 
+
+    /** 
+     * ArrayList 출력; 
+     */ 
+    public void lottoPrint() { 
+        System.out.print("당첨번호는 :\t"); 
+        for (String ran : /* 배열이름 */this) { 
+            System.out.print(ran + "\t"); 
+        } 
+        System.out.println(); 
+    } 
+} 
+
---
+++ egov.test1/src/main/java/example/game/lotto/EachNum.java
@@ -0,0 +1,42 @@
+package example.game.lotto;
+
+import java.awt.*;
+import javax.swing.*;
+ 
+public class EachNum extends JPanel {
+    private int lotNum[];   
+    JLabel title[], lotLabel[];
+     
+    public EachNum() {
+        this.setLayout(new GridLayout(2,7));
+         
+        title = new JLabel[7];
+        for(int i=0; i<7; i++) {
+            if(i == 6) {
+                title[i] = new JLabel("Bonus");
+                this.add(title[i]);
+            } else {
+                title[i] = new JLabel((i+1)+"th");
+                this.add(title[i]);
+            } // if.. else
+            title[i].setHorizontalAlignment(SwingConstants.CENTER);
+            title[i].setFont(new Font("Monofur",Font.PLAIN,14));
+        } // for i
+         
+        lotLabel = new JLabel[7];
+        for(int i=0; i<7; i++) {
+            lotLabel[i] = new JLabel();
+            lotLabel[i].setVisible(false);
+            lotLabel[i].setHorizontalAlignment(SwingConstants.CENTER);
+            lotLabel[i].setVerticalAlignment(SwingConstants.CENTER);
+            lotLabel[i].setFont(new Font("Monofur",Font.BOLD,14));
+            lotLabel[i].setForeground(Color.darkGray);
+            this.add(lotLabel[i]);
+        } // for i
+    }   // EachNum()
+ 
+       public void makeNum() {      
+    }   
+} // EachNum class
+
+
